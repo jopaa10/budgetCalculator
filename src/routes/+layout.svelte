87f92b3a4c7d098f469094b85929739f256dbc14
Josipa Znaor <josipaznaor@MacBook-Pro-od-Josipa.fritz.box>
2022-12-19 09:35:45 +0100

    adding title as component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<div class="app">
+	<slot />
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Title from '$lib/components/Title.svelte';
+</script>
+
+<svelte:head>
+	<title>Budget Calculator</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<Navbar />
+<Title title={'Add Expense'} />
+<Title title={'Expense'} />
